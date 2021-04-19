7358cad4b1aa8e49d7122f93ff26e25122672615
Marcriko <78810625+Marcriko@users.noreply.github.com>
2021-04-19 13:22:58 -0500

    falla 1

--- src/main/java/com/webappsecurity/security/controller/PlatilloController.java
+++ src/main/java/com/webappsecurity/security/controller/PlatilloController.java
@@ -1,9 +1,6 @@
 package com.webappsecurity.security.controller;
 
-<<<<<<< HEAD
 import java.io.IOException;
-=======
->>>>>>> parent of 0f711a2 (imagen)
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-<<<<<<< HEAD
 import org.springframework.web.multipart.MultipartFile;
-=======
->>>>>>> parent of 0f711a2 (imagen)
 
 import com.webappsecurity.security.entity.PlatilloEntity;
 import com.webappsecurity.security.jwt.JwtProvider;
@@ -28,45 +23,43 @@ import com.webappsecurity.security.service.PlatilloService;
 @RestController
 @RequestMapping("/resto")
 //@PreAuthorize("hasAuthority('ROLE_USER')")
-@CrossOrigin(origins = "*",methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT},allowedHeaders = "*")
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE,
+		RequestMethod.PUT }, allowedHeaders = "*")
 public class PlatilloController {
 
 	@Autowired
 	PlatilloService platilloService;
-	
+
 	@GetMapping("/platillo/findAll")
-	public List<PlatilloEntity> findAll(){
+	public List<PlatilloEntity> findAll() {
 		return platilloService.platilloEntities();
 	}
-	
-	
+
 	@GetMapping("/platillo/findByName/{param}")
-	public List<PlatilloEntity> findByName(@PathVariable("param") String param){
+	public List<PlatilloEntity> findByName(@PathVariable("param") String param) {
 		return platilloService.getLikeEntities(param);
 	}
-	
-	
+
 	@GetMapping("/platillo/findOne/{id}")
 	public PlatilloEntity findOne(@PathVariable("id") int id) {
 		return platilloService.getPlatilloEntity(id);
 	}
-	
-	@PostMapping("/platillo/save")
-<<<<<<< HEAD
-	public boolean save(@RequestBody PlatilloEntity platillo, @RequestParam("file") MultipartFile file) throws IOException {
+
+	/*@PostMapping("/platillo/save")
+	public boolean save(@RequestBody PlatilloEntity platillo, @RequestParam("file") MultipartFile file)
+			throws IOException {
 		System.out.println(file.getOriginalFilename());
-=======
+	}*/
+
 	public boolean save(@RequestBody PlatilloEntity platillo) {
-		
->>>>>>> parent of 0f711a2 (imagen)
+
 		return platilloService.save(platillo);
 	}
-	
+
 	@DeleteMapping("/platillo/delete/{id}")
 	public boolean delete(@PathVariable("id") int id) {
-		
+
 		return platilloService.delete(id);
 	}
-	
-	
+
 }
